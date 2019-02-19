1da3592a3ac08d48e17ecb74f7752ae5a48f7229
melZula <zula0747@gmail.com>
2019-02-19 16:56:42 +0300

    DFS

---
+++ lab2.2/1/topsort.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector< vector<int> > G;
+vector<int> used;
+
+void BFS(int s, int iter) {
+  s--;
+  queue<int> q;
+  used[s] = iter;
+  q.push(s);
+  while (!q.empty()) {
+    int v = q.front();
+    q.pop();
+    for (size_t i = 0; i < G[v].size(); i++) {
+      int to = G[v][i];
+      if (!used[to]) {
+        q.push(to);
+        used[to] = iter;
+      }
+    }
+  }
+}
+
+void DFS (int v) {
+  printf("%d\n", v );
+	used[v] = true;
+	for (size_t i = 0; i < G[v].size(); i++) {
+		if (!used[G[v][i]])
+			DFS (G[v][i]);
+  }
+}
+
+int main() {
+  ifstream fin("topsort.in");
+  ofstream fout("topsort.out");
+
+  int V, E;
+  fin >> V >> E;
+
+  G.resize(V);
+  used.resize(V, 0);
+  int answ[E];
+
+  int i, j, x, y;
+  for (i = 0; i < E; i++) {
+    fin >> x >> y;
+    G[x-1].push_back(y-1);
+    G[y-1].push_back(x-1);
+  }
+
+  for (i = 0; i < V; i++) {
+    printf("%d: ", i);
+    for (j = 0; j < G[i].size(); j++) {
+      printf("%d ", G[i][j] );
+    }
+    printf("\n" );
+  }
+
+  DFS(0);
+
+  for (j = 0; j < V; j++) {
+    fout << used[j] << ' ';
+  }
+  return 0;
+}
