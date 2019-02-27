8a62519c2e1e9daf9a9aa182120666b63e1356f9
melZula <zula0747@gmail.com>
2019-02-27 10:43:09 +0300

    DFS

--- lab2.2/4/cond.cpp
+++ lab2.2/4/cond.cpp
@@ -8,10 +8,12 @@
 using namespace std;
 
 vector< vector<int> > G;
+vector< vector<int> > Ginv;
 vector<int> used;
 stack<int> times;
 vector<int> answ;
 int tout = 0;
+int iter = 1;
 
 void timeDFS (int v) {
   used[v] = 1;
@@ -19,30 +21,36 @@ void timeDFS (int v) {
     if (!used[G[v][i]]) timeDFS (G[v][i]);
   }
   times.push(v);
-  //printf("%ld -- %d\n", times.size(), times.top()+1);
 }
 
-void DFS (int v) {
+void DFS (int v) { // inversed
   used[v] = 1;
-  for (int i = 0; i < G[v].size(); i++) {
-    if (!used[G[v][i]]) DFS (G[v][i]);
+  for (int i = 0; i < Ginv[v].size(); i++) {
+    if (!used[Ginv[v][i]]) DFS (Ginv[v][i]);
   }
-  answ.push_back(v);   // HERE WTF
+  answ[v] = iter;
 }
 
 void topsort(int V) {
   answ.clear();
   for (int i=0; i < V; ++i) {
     if (!used[i]) {
-      DFS (i);
+      timeDFS (i);
     }
   }
 }
 
 void condition(int V) {
   topsort(V);
+  used.clear();
   used.resize(V, 0);
-  // HERE
+  for (int i = 0; i < V; i++) {
+    if (!used[times.top()]) {
+      DFS(times.top());
+      iter++;
+    }
+    times.pop();
+  }
 }
 
 int main() {
@@ -53,20 +61,22 @@ int main() {
   fin >> V >> E;
 
   G.resize(V);
+  Ginv.resize(V);
   used.resize(V, 0);
-  answ.resize(V);
+  answ.resize(V, 0);
 
   int i, j, x, y;
   for (i = 0; i < E; i++) {
     fin >> x >> y;
     G[x-1].push_back(y-1);
+    Ginv[y-1].push_back(x-1);
   }
 
-  topsort(V); // HERE
+  condition(V);
 
-  for (j = 0; j > times.size(); j++) { // HERE
-    fout << times.top() << ' ';
-    times.pop();
+  fout << iter - 1 << endl;
+  for (size_t i = 0; i < V; i++) {
+    fout << answ[i] << ' ';
   }
 
   return 0;
