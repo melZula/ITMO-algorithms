15cfec43d55179e68c52b970309e020d9dd88bbc
Petr Melentev <41367520+melZula@users.noreply.github.com>
2019-03-03 19:57:21 +0300

    DFS

--- lab2.2/5/hamiltonian.cpp
+++ lab2.2/5/hamiltonian.cpp
@@ -8,32 +8,48 @@ using namespace std;
 
 vector< vector<int> > G;
 vector<int> used;
-vector<int> group;
+vector<int> answ;
 bool flag = false;
 
-void BFS(int s) {
-  s--;
-  queue<int> q;
-  used[s] = 1;
-  group[s] = 1; // 1, -1 group
-  q.push(s);
-  while (!q.empty()) {
-    int v = q.front();
-    q.pop();
-    for (size_t i = 0; i < G[v].size(); i++) {
-      int to = G[v][i];
-      if (group[to] != group[v]) {
-        group[to] = -group[v];
-      } else flag = true;
-      if (!used[to]) {
-        q.push(to);
-        used[to] = 1;
-      }
+
+void DFS (int v) {
+  used[v] = 1;  // 1 - gray 2 - black
+  int i;
+  for (i = 0; i < G[v].size(); i++) {
+    if (used[G[v][i]] == 1) {
+      flag = 1;
+    } else {
+      if (used[G[v][i]] == 0) DFS (G[v][i]);
+    }
+  }
+  used[v] = 2;
+  answ.push_back(v);
+}
+
+void topsort(int V) {
+  answ.clear();
+  for (int i=0; i < V; ++i) {
+    if (!used[i]) {
+      DFS (i);
     }
   }
 }
 
-void 
+bool hamiCheck(int V) {
+  bool check = true;
+  for (int i = 0; i < V-1; i++) {
+    int v = answ[V - i -1];
+    if (check) {
+      check = false;
+      for (int j = 0; j < G[v].size(); j++) {
+        if (G[v][j] == answ[V-i-2]) {
+          check = true;
+        }
+      }
+    } else break;
+  }
+  return check;
+}
 
 int main() {
   ifstream fin("hamiltonian.in");
@@ -44,7 +60,7 @@ int main() {
 
   G.resize(V);
   used.resize(V, 0);
-  group.resize(V, 0);
+  answ.resize(V, 0);
 
   int i, j, x, y;
   for (i = 0; i < E; i++) {
@@ -52,13 +68,9 @@ int main() {
     G[x-1].push_back(y-1);
   }
 
-  for (i = 0; i < V; i++) {
-    if (!used[i]) {
-      BFS(i + 1);
-    }
-  }
+  topsort(V);
 
-  fout << (flag ? "NO" : "YES");
+  fout << (hamiCheck(V) ? "YES" : "NO");
 
   return 0;
 }
